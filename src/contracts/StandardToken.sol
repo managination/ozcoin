5fc97373c544794ccfad4307bb9a869be2798334
Laurence Kirk <laurence@extropy.io>
2017-02-24 15:18:22 +0000

    allow ether withdrawal

--- src/contracts/StandardToken.sol
+++ src/contracts/StandardToken.sol
@@ -12,19 +12,27 @@ import "User.sol";
 
 contract StandardToken is ExchangeToken {
 
+  struct Price{
+      uint256 buy;
+      uint256 sell;
+  }
 
 User userContract;
+address ozCoinAccount;
+
 mapping (address=>uint256) etherBalances;
 
+mapping (address=>Price) prices;
 
 event TransactionFeeChanged(uint256 oldRate,uint256 newRate);
-event OzCoinPaid(address _sender,uint256 _amount);
-event AffiliatePaid(address _sender,address _affiliate,uint256 _amount);
-event InsufficientFunds(address _account,uint256 _offered, uint256 _required);
+event OzCoinPaid(address indexed _sender,uint256 _amount);
+event AffiliatePaid(address indexed _sender,address _affiliate,uint256 _amount);
+event InsufficientFunds(address indexed _account,uint256 _offered, uint256 _required);
 
-event AccountFrozen(address _account);
-event AccountUnFrozen(address _account);
-event ArbitrationRequested(address _account,bytes32 ID);
+event AccountFrozen(address indexed _account);
+event AccountUnFrozen(address indexed _account);
+event ArbitrationRequested(address indexed  _account,bytes32 ID);
+event PriceSet(address indexed seller,bool side,uint256 price);
 
 
 
@@ -36,6 +44,10 @@ function resetUser(User _userContract) onlyowner {
   userContract = _userContract;
 }
 
+function resetTokenData(TokenData _tokenData) onlyowner {
+  tokenData = _tokenData;
+  ozCoinAccount = tokenData.getOzCoinAccount();
+}
 
     // modifier only admin
   function setTransactionFee(uint256 _fee) returns(bool success){
@@ -48,22 +60,24 @@ function resetUser(User _userContract) onlyowner {
 
 
   // modifiers needed
-  // checks seller has sufficient coins
   // check buyer has enough ether for coin plus fee
-
+  // need to check amount is greater than fee
    function buyCoins (uint256 _amount,address _seller) payable returns (bool success){
      uint256 fee = calculateTransactionFee(_amount);
      uint256 sellPrice;
-     (sellPrice,)  = tokenData.getPrices(_seller);
+     (sellPrice,)  = getPrices(_seller);
 
-     if(fee+sellPrice > _amount){
-       InsufficientFunds(msg.sender,_amount,fee+sellPrice);
+     if(_amount<=fee || sellPrice <= 0){
        return (false);
+     }
+
+     if(sellPrice > msg.value){
+      InsufficientFunds(msg.sender,_amount,fee+sellPrice);
+      return (false);
         }
      else{
 
-      address ozCoinAccount = tokenData.getOzCoinAccount();
-      // bool sendFlag =  ozCoinAccount.send(msg.value);
+
       address affiliateAccount;
       address affiliateCompany;
 
@@ -75,30 +89,32 @@ function resetUser(User _userContract) onlyowner {
        uint256 affiliateShare = 5 * msg.value / 100;
        uint256 ozShare = msg.value;
 
-       if(affiliateAccount!=0x0){
-         etherBalances[affiliateAccount] = affiliateShare;
+      if(affiliateAccount!=0x0){
+         etherBalances[affiliateAccount] += affiliateShare;
          ozShare = ozShare - affiliateShare;
          AffiliatePaid(msg.sender,affiliateAccount,affiliateShare);
-       }
-       if(affiliateCompany!=0x0){
-         etherBalances[affiliateCompany] = affiliateShare;
+      }
+      if(affiliateCompany!=0x0){
+         etherBalances[affiliateCompany] += affiliateShare;
          ozShare = ozShare - affiliateShare;
          AffiliatePaid(msg.sender,affiliateCompany,affiliateShare);
-       }
+      }
 
-       tokenData.transfer(ozCoinAccount,msg.sender,_amount,0);
+        tokenData.transfer(_seller,msg.sender,_amount-fee,0);
+        Transfer(_seller,msg.sender, _amount-fee);
+        // send fee to ozcoin
+        tokenData.transfer(_seller,ozCoinAccount,fee,0);
+        Transfer(_seller,msg.sender, fee);
 
-       //check return value
-       Transfer(ozCoinAccount,msg.sender, fee);
-
-       bool sendSuccess =  ozCoinAccount.send(ozShare);
-       OzCoinPaid(msg.sender,ozShare);
-
+       bool sendSuccess =  _seller.send(ozShare);
+       if(_seller==ozCoinAccount){
+            OzCoinPaid(msg.sender,ozShare);
+        }
      }
    }
 
 
-function getAffiliateInfo(address _account) returns (address,address){
+function getAffiliateInfo(address _account) constant returns (address,address){
   address affiliate = userContract.getAffiliate(_account);
   address company = userContract.getAffiliateCompany(_account);
 
@@ -111,16 +127,29 @@ function getAffiliateBalance() constant returns(uint256){
 }
 
 function withdrawEther () returns (bool success){
-
+        uint amount = etherBalances[msg.sender];
+        etherBalances[msg.sender] = 0;
+        if (msg.sender.send(amount)) {
+            return true;
+        } else {
+            etherBalances[msg.sender] = amount;
+            return false;
+        }
 }
 
+function getPrices(address _seller) constant returns (uint256,uint256) {
+      return (prices[_seller].buy,prices[_seller].sell);
+}
 
 function setPrice(bool _isBuy,uint256 _price) {
-  tokenData.setPrice(msg.sender,_isBuy,_price);
-}
+    if(_isBuy){
+      prices[msg.sender].buy = _price;
+    }
+    else{
+      prices[msg.sender].sell = _price;
+    }
+    PriceSet(msg.sender,_isBuy,_price);
 
-function getPrices(address _user) constant returns(uint256 _buyPrice,uint256 _sellPrice){
-  return tokenData.getPrices(_user);
 }
 
 //Admin functions
